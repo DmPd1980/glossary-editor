49e1027d8a52333a1cebf6685ea5363950a4f857
DmPd1980 <pdv1001@gmail.com>
2025-04-21 13:23:38 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import Editor from './Editor';
+
+function App() {
+  return (
+    <div>
+      <h2 style={{ textAlign: "center", margin: "1em 0" }}>Редактор с глоссарием</h2>
+      <Editor />
+    </div>
+  );
+}
+
+export default App;
 
