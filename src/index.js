811f43e9295df2844a3647e31b6eb86c4c7be9ee
DmPd1980 <pdv1001@gmail.com>
2025-04-21 21:23:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App'; // Импортируем компонент App
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Glossary Editor</h1>
+    <App /> {/* Рендерим компонент App */}
   </React.StrictMode>
 );
