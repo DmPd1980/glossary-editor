9df3bd70eefe1f67555a3ef1bbcf641ab4672f75
DmPd1980 <pdv1001@gmail.com>
2025-04-21 23:58:24 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { loadGlossary } from './GlossaryService';
 import Editor from './Editor';
 
 function App() {
+  const [glossary, setGlossary] = React.useState([]);
+
+  React.useEffect(() => {
+    loadGlossary().then(data => {
+      console.log('Глоссарий загружен:', data); // Логирование
+      setGlossary(data);
+    });
+  }, []);
+
   return (
     <div>
-      <h2>Редактор с глоссарием</h2> {/* Оставляем здесь заголовок */}
-      <Editor />
+      <h2>Редактор с глоссарием</h2> {/* Оставляем заголовок здесь */}
+      <Editor glossary={glossary} />
     </div>
   );
 }
