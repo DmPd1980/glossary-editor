7884d642dc64b8d57dd9c8f6b104f6f9cf3072dc
DmPd1980 <pdv1001@gmail.com>
2025-04-21 13:22:57 +0300

    Update GlossaryService.js

--- src/GlossaryService.js
+++ src/GlossaryService.js
@@ -1,1 +1,9 @@
+import glossary from './glossary.json';
+
+export function findGlossaryMatches(text) {
+  return glossary.filter(entry => {
+    const regex = new RegExp(`\\b${entry.term}\\b`, 'gi');
+    return regex.test(text);
+  });
+}
 
